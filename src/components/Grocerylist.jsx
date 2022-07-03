4d151fd6cf53bc0c8093fa63b52b439abe295827
atharva-tribhuwane <tribhuwane.a@gmail.com>
2022-07-04 00:31:05 +0530

    final

--- src/components/Grocerylist.jsx
+++ src/components/Grocerylist.jsx
@@ -2,16 +2,38 @@ import React from "react";
 import { useState } from "react";
 const Grocerylist=()=>{
 
-    React.useEffect(()=>{
+    const [data, setData] = useState([]);
+
+    const displaydata=()=>{
         fetch(`http://localhost:3001/list`)
          .then((res)=>res.json())
+         .then((res)=>setData(res))
+         .catch((err)=>console.log(err))
+    }
+
+    const handledelete = (grocid)=>{
+        console.log(grocid)
+        fetch(`http://localhost:3001/tasks/${grocid}`,{
+            method:"DELETE"
+        })
+         .then((res)=>{res.json()})
          .then((res)=>console.log(res))
-    },[])
+         .catch((err)=>console.log(err))
+    }
+
+    React.useEffect(()=>{
+      displaydata();
+    },[]);
 
     return(
         <>
-        <h1>GroceryList</h1>
-        
+        <h1>List: </h1>
+        {data.map((groc) => (
+        <div style={{display:"flex", width:"50%", border:"1px solid red", justifyContent:"space-between"}}>
+        <h1 key={groc.id}>{groc.task}</h1>
+        <button onClick={()=>handledelete(groc.id)}>Delete</button>
+        </div>
+      ))}
         </>
     )
 }
